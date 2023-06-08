e6d70bd33ec044755a6034e29fcae856b32b73d2
sheephy <jackhsu1995@gmail.com>
2023-06-08 19:48:25 +0800

    fix: adding model

--- code/model.R
+++ code/model.R
@@ -1,8 +1,10 @@
 library(ggplot2)
 library(dplyr)
-library(fitdistrplus)
 library(magrittr)
-library(outliers)
+library(MASS)
+library(caret)
+library(janitor)
+library(xgboost)
 
 
 train <- read.table("data/train.csv", sep = ",", header = TRUE)
@@ -14,14 +16,6 @@ train <- train[, -1]
 test <- test[, -1]
 
 
-
-# summary(train)
-# outlier(train$GrLivArea)
-
-# col_types <- sapply(train, class)
-# for (i in which(col_types == "integer")) {
-#    print(grubbs.test(train[, i])[3] < 0.05)
-# }
 
 # Plot the distribution
 ggplot(train, aes(x = SalePrice)) +
@@ -75,7 +69,6 @@ ntest <- nrow(test)
 y_train <- train$SalePrice
 all_data <- bind_rows(train, test)  # 合併資料集
 all_data <- all_data[!names(all_data) == "SalePrice"]  # 刪除 SalePrice 欄位
-cat(paste("all_data size is :", dim(all_data), "\n"))
 
 all_data_na <- (colSums(is.na(all_data)) / nrow(all_data)) * 100
 all_data_na <- all_data_na[all_data_na > 0]
@@ -93,17 +86,19 @@ box()
 # 用 "None" 或 0 填充缺失值
 none_col <- c("PoolQC", "MiscFeature", "Alley", "Fence", "FireplaceQu"
   , "MasVnrType", "GarageType", "GarageFinish", "GarageQual", "GarageCond"
-  , "BsmtQual", "BsmtCond", "BsmtExposure", "BsmtFinType1", "BsmtFinType2")
+  , "BsmtQual", "BsmtCond", "BsmtExposure", "BsmtFinType1", "BsmtFinType2"
+)
 
 all_data[, none_col] <- lapply(all_data[, none_col]
   , function(x) ifelse(is.na(x), "None", x))
 
 zero_col <- c("MasVnrArea", "GarageYrBlt", "GarageArea"
   , "GarageCars", "BsmtFinSF1", "BsmtFinSF2", "BsmtUnfSF"
-  , "TotalBsmtSF", "BsmtFullBath", "BsmtHalfBath")
+  , "TotalBsmtSF", "BsmtFullBath", "BsmtHalfBath"
+)
 
 all_data[, zero_col] <- lapply(all_data[, zero_col]
-  , function(x) ifelse(is.na(x), "None", x))
+  , function(x) ifelse(is.na(x), 0, x))
 
 # 缺失較少的類別型特徵，用眾數填補缺失值
 
@@ -111,10 +106,109 @@ mode_col <- c("MSZoning", "Electrical", "KitchenQual"
   , "Exterior1st", "Exterior2nd", "SaleType")
 
 mode_naomit <- function(x) {
-  ux <- na.omit(unique(x) )
+  ux <- na.omit(unique(x))
   tab <- tabulate(match(x, ux))
-  ux[tab == max(tab) ]
+  ux[tab == max(tab)]
 }
 
 all_data[, mode_col] <- lapply(all_data[, mode_col]
   , function(x) ifelse(is.na(x), mode_naomit(x), x))
+
+
+# 以各個房屋所在鄰域（Neighborhood）的LotFrontage中位數作為填補值
+
+all_data$LotFrontage <- ave(all_data$LotFrontage, all_data$Neighborhood
+  , FUN = function(x) ifelse(is.na(x), median(x, na.rm = TRUE), x))
+
+# 用 "Typ" 填補 "Functional" 欄位的缺失值
+all_data$Functional <- ifelse(is.na(all_data$Functional)
+  , "Typ", all_data$Functional)
+
+# 刪除 "Utilities" 欄位，因其方差非常小
+all_data <- subset(all_data, select = -c(Utilities))
+
+# 最後確認是否已處理完所有缺失值
+sum(is.na(all_data))
+
+# 將數值型特徵轉換為類別型特徵
+all_data$MSSubClass <- as.character(all_data$MSSubClass)
+all_data$YrSold <- as.character(all_data$YrSold)
+all_data$MoSold <- as.character(all_data$MoSold)
+
+# 將特徵的類別映射為有大小的數字
+ordinal_mapping <- list(
+  "Ex" = 5, "Gd" = 4, "TA" = 3, "Fa" = 2, "Po" = 1, "None" = 0,
+  "Fin" = 3, "RFn" = 2, "Unf" = 1, "Av" = 3,
+  "GLQ" = 6, "ALQ" = 5, "BLQ" = 4, "Rec" = 3, "LwQ" = 2, "Unf" = 1,
+  "Typ" = 8, "Min1" = 7, "Min2" = 6, "Mod" = 5,
+  "Maj1" = 4, "Maj2" = 3, "Sev" = 2, "Sal" = 1,
+  "GdPrv" = 4, "MnPrv" = 3, "GdWo" = 2, "MnWw" = 1,
+  "Gtl" = 3, "Mod" = 2, "Sev" = 1,
+  "Reg" = 4, "IR1" = 3, "IR2" = 2, "IR3" = 1,
+  "Y" = 3, "P" = 2, "N" = 1,
+  "Pave" = 2, "Grvl" = 1
+)
+
+
+ordinal_cols <- c(
+  "FireplaceQu", "GarageQual", "GarageCond", "GarageFinish", "BsmtQual"
+  , "BsmtCond", "BsmtExposure", "BsmtFinType1", "BsmtFinType2"
+  , "ExterQual", "ExterCond", "HeatingQC", "PoolQC"
+  , "KitchenQual", "Functional", "Fence", "LandSlope"
+  , "LotShape", "PavedDrive", "Street", "Alley", "CentralAir"
+)
+
+for (col in ordinal_cols) {
+  all_data[[col]] <- recode(all_data[[col]], !!!ordinal_mapping, .default = 0)
+}
+
+numeric_feats <- names(all_data)[sapply(all_data, is.numeric)]
+character_feats <- names(all_data)[sapply(all_data, is.character)]
+dummy_variables <- dummyVars("~.", data = all_data[, character_feats])
+
+# 套用變數轉換規則
+encoded_data <- as.data.frame(predict(dummy_variables, newdata = all_data))
+
+# 合併原始數據與 One-Hot Encoding 結果
+all_data_encoded <- cbind(all_data[c(numeric_feats)], encoded_data)
+
+names(all_data_encoded) <- gsub(" ", "_", names(all_data_encoded))
+names(all_data_encoded) <- gsub("\\.", "_", names(all_data_encoded))
+names(all_data_encoded) <- gsub("\\(", "_", names(all_data_encoded))
+names(all_data_encoded) <- gsub("\\)", "_", names(all_data_encoded))
+
+train_data <- all_data_encoded[train_id, ]
+test_data <- all_data_encoded[test_id, ]
+
+# train_data$SalePrice <- y_train
+
+
+model <- glm(
+  SalePrice ~ .
+  , data = train_data
+)
+
+
+model_xg <- xgboost(
+  data = as.matrix(train_data)
+  , label = y_train
+  , nrounds = 500
+)
+
+
+
+
+
+output <- data.frame(
+  ID = test_id
+  , SalePrice = expm1(predict(model_xg, as.matrix(test_data)))
+)
+
+
+
+write.table(output, file = "output.csv"
+    , sep = ","
+    , col.names = TRUE
+    , quote = FALSE
+    , row.names = FALSE
+)
